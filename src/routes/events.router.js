eae71483474ad29d8251dde7c6f9eed260f49d07
Young-Hyun Kwon <ehe151@naver.com>
2024-09-23 20:19:38 +0900

    [FEAT] 자동매치메이킹 API 구현 완료 - #23
    [FEAT] 자동매치메이킹 API 구현 완료
    
    1) prisma의 raw query를 사용하여 복잡한 쿼리문을 작성해봤습니다.
        - 자신의 Rating과 가장 가까운 다른 유저들을 최대 10명 조회하는 쿼리문 작성
    
    2) 비슷한 Rating의 다른 유저의 팀들 중 랜덤하게 한 팀을 선택하여 게임 플레이를 한다.
    
    3) 게임 플레이 로직은 #8과 동일합니다. 참고바랍니다

--- src/routes/events.router.js
+++ src/routes/events.router.js
@@ -14,7 +14,10 @@ import {
 } from "../utils/logic/game.js";
 import { createMatchLogToDB } from "../utils/prisma/mathLogs.prisma.js";
 import { findPlayerFromDB } from "../utils/prisma/players.prisma.js";
-import { findTeamFromDB } from "../utils/prisma/teams.prisma.js";
+import {
+  findTeamFromDB,
+  findTeamsAutoMatchMakingFromDB,
+} from "../utils/prisma/teams.prisma.js";
 import {
   findUserFromDB,
   updateUserRatingToDB,
@@ -170,10 +173,15 @@ router.patch("/events/autoMatching/:teamId", async (req, res, next) => {
   try {
     const userId = 1; // req.header.authorization
     const { teamId: myTeamId } = await teamIdParamValidate(req.params);
-    const { enemyTeamId } = await teamIdBodyValidate(req.body);
+
+    const user = await findUserFromDB(userId);
+    const enemyTeams = await findTeamsAutoMatchMakingFromDB(
+      userId,
+      user.rating,
+    );
 
     const myTeam = await findTeamFromDB(myTeamId);
-    //const enemyTeam = await findTeamFromDB(enemyTeamId);
+    const enemyTeam = enemyTeams[Math.floor(Math.random() * enemyTeams.length)];
 
     // 내팀이 DB에 없는 경우
     if (!myTeam)
@@ -188,6 +196,98 @@ router.patch("/events/autoMatching/:teamId", async (req, res, next) => {
       throw new BadRequestError(
         "내 팀의 인원 수가 3명 미만이기 때문에 게임을 진행하실 수 없습니다.",
       );
+
+    const myTeamArr = [];
+    const enemyTeamArr = [];
+
+    for (let i = 1; i < 4; i++) {
+      const myPlayerId = myTeam[`UserPlayer` + i].playerId;
+      const myPlayer = await findPlayerFromDB(myPlayerId);
+      const myPlayerUpgrade = myTeam[`UserPlayer` + i].upgrade;
+
+      // 본인 플레이어 정보가 존재하지 않을 경우
+      if (!myPlayer)
+        throw new NotFoundError(
+          `내 팀 선수 중 (${myPlayerId}) 의 선수 데이터가 존재하지 않습니다.`,
+        );
+
+      myTeamArr.push({ player: myPlayer, upgrade: myPlayerUpgrade });
+
+      const enemyPlayerId = enemyTeam[`playerId` + i];
+      const enemyPlayer = await findPlayerFromDB(enemyPlayerId);
+      const enemyPlayerUpgrade = enemyTeam[`upgrade` + i];
+
+      // 상대 플레이어 정보가 존재하지 않을 경우
+      if (!myPlayer)
+        throw new NotFoundError(
+          `상대 팀 선수 중 (${enemyPlayerId}) 의 선수 데이터가 존재하지 않습니다.`,
+        );
+
+      enemyTeamArr.push({ player: enemyPlayer, upgrade: enemyPlayerUpgrade });
+    }
+
+    const myTeamScore = getTeamScore(myTeamArr);
+    const enemyTeamScore = getTeamScore(enemyTeamArr);
+
+    const matchResult = getMatchResult(myTeamScore, enemyTeamScore);
+
+    // 경기 결과에 대해 Rating 점수를 자신과 상대팀의 유저에게 반영한다.
+    // Rating 점수는 기본 10점이다.
+    // 불리한 조건(내 팀의 총 점수가 상대팀의 총 점수보다 낮을 때)
+    //    - 승리할 경우 : 기본 10점보다 더 준다.
+    //    - 패배할 경우 :
+    // 반면에, 유리한 조건일 경우 => 10점보다 덜 준다.
+    let changeRatingScore = getBonusRatingScore(
+      myTeamScore,
+      enemyTeamScore,
+      matchResult.result,
+    );
+
+    let myUser = null;
+    let enemyUser = null;
+    let message = "";
+
+    switch (matchResult.result) {
+      case MATCH_RESULT.WIN:
+        {
+          myUser = await updateUserRatingToDB(myTeam.userId, changeRatingScore);
+          enemyUser = await updateUserRatingToDB(
+            enemyTeam.userId,
+            -changeRatingScore,
+          );
+          message = `[${matchResult.score}] 로 승리하셨습니다!!! Rating이 ${changeRatingScore} 증가했습니다.`;
+        }
+        break;
+      case MATCH_RESULT.DRAW:
+        {
+          myUser = await findUserFromDB(myTeam.userId);
+          message = `[${matchResult.score}] 로 비겼습니다!!!`;
+        }
+        break;
+      case MATCH_RESULT.LOSE:
+        {
+          myUser = await updateUserRatingToDB(
+            myTeam.userId,
+            -changeRatingScore,
+          );
+          enemyUser = await updateUserRatingToDB(
+            enemyTeam.userId,
+            changeRatingScore,
+          );
+          message = `[${matchResult.score}] 로 패배하셨습니다... Rating이 ${changeRatingScore} 감소했습니다.`;
+        }
+        break;
+    }
+
+    // 경기 결과를 매치로그에 저장한다.
+    // - 우선 내 팀 기준의 MatchLog 만 저장하도록 한다.
+    const matchLog = await createMatchLogToDB(
+      myTeam.userId,
+      enemyTeam.userId,
+      matchResult.result,
+    );
+
+    return res.status(200).json({ message, rating: myUser.rating });
   } catch (error) {
     next(error);
   }
